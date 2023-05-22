5b9659ed1775a40528910cc3cc77b6f138c37056
Mirt12 <Mirt12@yandex.ru>
2023-05-22 16:54:42 +0200

    add 3 tests to TripadvisorPageTest class

--- src/test/java/by/itacademy/tatjana/balashevich/TripadvisorPage.java
+++ src/test/java/by/itacademy/tatjana/balashevich/TripadvisorPage.java
@@ -3,10 +3,8 @@ package by.itacademy.tatjana.balashevich;
 public class TripadvisorPage {
     String btnCookiesXPath = "//*[@id='onetrust-accept-btn-handler']";
     String btnSignInXPath = "//span[contains(text(),'Sign in')]";
-
-    //iframe
     String btnContinueWithEmailXPath = "//span[contains(text(),'Continue with email')]";
     String inputEmailXPath = "//input[@id='regSignIn.email']";
-    String inputPasswordXPath = "//input[@id='regSignIn.email']";
+    String inputPasswordXPath = "//input[@id='regSignIn.password']";
     String btnSingInFrameXPath = "//div[@id='regBody']//button[@type='submit' and contains(text(),'Sign in')]";
 }
--- src/test/java/by/itacademy/tatjana/balashevich/TripadvisorTest.java
+++ src/test/java/by/itacademy/tatjana/balashevich/TripadvisorTest.java
@@ -14,16 +14,9 @@ import java.time.Duration;
 import java.util.function.Function;
 
 public class TripadvisorTest {
+    //access to variables of not created object:
     TripadvisorPage tripadvisorPage = new TripadvisorPage();
     ChromeOptions options = new ChromeOptions();
-
-    private WebElement elementByXpath(Wait<WebDriver> wait, String xpath) {
-        return wait.until(new Function<WebDriver, WebElement>() {
-            public WebElement apply(WebDriver driver) {
-                return driver.findElement(By.xpath(xpath));
-            }
-        });
-    }
 
     @Test
     public void enterWithEmptyEmailAndEmptyPassword() throws InterruptedException {
@@ -86,6 +79,200 @@ public class TripadvisorTest {
         });
         btnSignInFrame.click();
 
+        driver.quit();
+    }
+
+    @Test
+    public void enterWithInvalidEmail() throws InterruptedException {
+        //open browser incognito and w/o cash:
+        options.addArguments("--incognito");
+        options.addArguments("--disable-cache");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.tripadvisor.com");
+
+        //fluent Wait- Set up:
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(30L))
+                .pollingEvery(Duration.ofSeconds(5L))
+                .ignoring(NoSuchElementException.class);
+
+        //fluent Wait - Search element:
+        WebElement btnCookies = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.btnCookiesXPath));
+            }
+        });
+        btnCookies.click();
+
+        WebElement btnSignIn = driver.findElement(By.xpath(tripadvisorPage.btnSignInXPath));
+        btnSignIn.click();
+
+        //go to iframe-element:
+        Thread.sleep(2000);
+        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='regcontroller']")));
+
+        //fluent Wait - Search element:
+        WebElement btnContinueWithEmail = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.btnContinueWithEmailXPath));
+            }
+        });
+        btnContinueWithEmail.click();
+
+        //fluent Wait - Search element:
+        WebElement inputEmail = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.inputEmailXPath));
+            }
+        });
+        inputEmail.sendKeys("sodab92025dekaps.com");
+
+        //fluent Wait - Search element:
+        WebElement inputPassword = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.inputPasswordXPath));
+            }
+        });
+        inputPassword.sendKeys("");
+
+        //fluent Wait - Search element:
+        WebElement btnSignInFrame = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.btnSingInFrameXPath));
+            }
+        });
+        btnSignInFrame.click();
+
+        driver.quit();
+    }
+
+    @Test
+    public void enterValidEmailAndEmptyPassword() throws InterruptedException {
+        //open browser incognito and w/o cash:
+        options.addArguments("--incognito");
+        options.addArguments("--disable-cache");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.tripadvisor.com");
+
+        //fluent Wait- Set up:
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(30L))
+                .pollingEvery(Duration.ofSeconds(5L))
+                .ignoring(NoSuchElementException.class);
+
+        //fluent Wait - Search element:
+        WebElement btnCookies = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.btnCookiesXPath));
+            }
+        });
+        btnCookies.click();
+
+        WebElement btnSignIn = driver.findElement(By.xpath(tripadvisorPage.btnSignInXPath));
+        btnSignIn.click();
+
+        //go to iframe-element:
+        Thread.sleep(2000);
+        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='regcontroller']")));
+
+        //fluent Wait - Search element:
+        WebElement btnContinueWithEmail = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.btnContinueWithEmailXPath));
+            }
+        });
+        btnContinueWithEmail.click();
+
+        //fluent Wait - Search element:
+        WebElement inputEmail = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.inputEmailXPath));
+            }
+        });
+        inputEmail.sendKeys("sodab92025@dekaps.com");
+
+        //fluent Wait - Search element:
+        WebElement inputPassword = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.inputPasswordXPath));
+            }
+        });
+        inputPassword.sendKeys("");
+
+        //fluent Wait - Search element:
+        WebElement btnSignInFrame = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.btnSingInFrameXPath));
+            }
+        });
+        btnSignInFrame.click();
+
+        driver.quit();
+    }
+
+    @Test
+    public void enterValidEmailAndInvalidPassword() throws InterruptedException {
+        //open browser incognito and w/o cash:
+        options.addArguments("--incognito");
+        options.addArguments("--disable-cache");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.tripadvisor.com");
+
+        //fluent Wait- Set up:
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(30L))
+                .pollingEvery(Duration.ofSeconds(5L))
+                .ignoring(NoSuchElementException.class);
+
+        //fluent Wait - Search element:
+        WebElement btnCookies = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.btnCookiesXPath));
+            }
+        });
+        btnCookies.click();
+
+        WebElement btnSignIn = driver.findElement(By.xpath(tripadvisorPage.btnSignInXPath));
+        btnSignIn.click();
+
+        //go to iframe-element:
+        Thread.sleep(2000);
+        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[title='regcontroller']")));
+
+        //fluent Wait - Search element:
+        WebElement btnContinueWithEmail = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.btnContinueWithEmailXPath));
+            }
+        });
+        btnContinueWithEmail.click();
+
+        //fluent Wait - Search element:
+        WebElement inputEmail = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.inputEmailXPath));
+            }
+        });
+        inputEmail.sendKeys("sodab92025@dekaps.com");
+
+        //fluent Wait - Search element:
+        WebElement inputPassword = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.inputPasswordXPath));
+            }
+        });
+        inputPassword.sendKeys("HHHHHHH");
+
+        //fluent Wait - Search element:
+        WebElement btnSignInFrame = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(tripadvisorPage.btnSingInFrameXPath));
+            }
+        });
+        btnSignInFrame.click();
+
         //driver.quit();
     }
+
+
 }
