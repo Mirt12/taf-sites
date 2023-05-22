c4646316c6a30cdf7ef355b8f210ab82c3828b72
Mirt12 <Mirt12@yandex.ru>
2023-05-22 22:19:29 +0200

    add DominosPage class

---
+++ src/main/java/by/itacademy/tatjana/balashevich/DominosPage.java
@@ -0,0 +1,9 @@
+package by.itacademy.tatjana.balashevich;
+
+public class DominosPage {
+
+    String btnVoityInHeader ="//button[@class='custom-button custom-button--primary custom-button--size-medium']";
+    String inputEmail = "//input[@type='email']";
+    String inputPassword = "//input[@type='password']";
+    String btnVoityInModal = "//button[@class='custom-button custom-button--secondary custom-button--full-width custom-button--size-medium']";
+}
