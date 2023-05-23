61b2373cd458594240464240ad4d24e7371d851a
Mirt12 <Mirt12@yandex.ru>
2023-05-23 13:49:17 +0200

    add 4 tests to PizzatempoPageTest class

--- src/test/java/by/itacademy/tatjana/balashevich/PizzatempoPageTest.java
+++ src/test/java/by/itacademy/tatjana/balashevich/PizzatempoPageTest.java
@@ -14,11 +14,11 @@ import java.time.Duration;
 import java.util.function.Function;
 
 public class PizzatempoPageTest {
-    PizzatempoPage pizzatempoPage=new PizzatempoPage();
+    PizzatempoPage pizzatempoPage = new PizzatempoPage();
     ChromeOptions options = new ChromeOptions();
 
     @Test
-    public void enterWithEmptyEmailAndEmptyPassword(){
+    public void enterWithEmptyEmailAndEmptyPassword() {
         //open browser incognito and w/o cash:
         options.addArguments("--incognito");
         options.addArguments("--disable-cache");
@@ -41,6 +41,114 @@ public class PizzatempoPageTest {
         WebElement btnSearch = driver.findElement(By.xpath(pizzatempoPage.btnSearch));
         btnSearch.click();
         //toDo
+        driver.quit();
     }
 
+    @Test
+    public void enterWithInvalidEmail() {
+        //open browser incognito and w/o cash:
+        options.addArguments("--incognito");
+        options.addArguments("--disable-cache");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.pizzatempo.by");
+        //fluent Wait- Set up:
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(30L))
+                .pollingEvery(Duration.ofSeconds(5L))
+                .ignoring(NoSuchElementException.class);
+        //fluent Wait - Search element:
+        WebElement inputEmail = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(pizzatempoPage.inputEmail));
+            }
+        });
+        inputEmail.sendKeys("@@@@@@");
+        WebElement inputPassword = driver.findElement(By.xpath(pizzatempoPage.inputPassword));
+        inputPassword.sendKeys("");
+        WebElement btnSearch = driver.findElement(By.xpath(pizzatempoPage.btnSearch));
+        btnSearch.click();
+        //toDo
+        driver.quit();
+    }
+
+    @Test
+    public void enterWithEmptyEmailAndAnyPassword() {
+        //open browser incognito and w/o cash:
+        options.addArguments("--incognito");
+        options.addArguments("--disable-cache");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.pizzatempo.by");
+        //fluent Wait- Set up:
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(30L))
+                .pollingEvery(Duration.ofSeconds(5L))
+                .ignoring(NoSuchElementException.class);
+        //fluent Wait - Search element:
+        WebElement inputEmail = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(pizzatempoPage.inputEmail));
+            }
+        });
+        inputEmail.sendKeys("");
+        WebElement inputPassword = driver.findElement(By.xpath(pizzatempoPage.inputPassword));
+        inputPassword.sendKeys("ZZZZZZ");
+        WebElement btnSearch = driver.findElement(By.xpath(pizzatempoPage.btnSearch));
+        btnSearch.click();
+        //toDo
+        driver.quit();
+    }
+
+    @Test
+    public void enterWithValidEmailAndEmptyPassword() {
+        //open browser incognito and w/o cash:
+        options.addArguments("--incognito");
+        options.addArguments("--disable-cache");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.pizzatempo.by");
+        //fluent Wait- Set up:
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(30L))
+                .pollingEvery(Duration.ofSeconds(5L))
+                .ignoring(NoSuchElementException.class);
+        //fluent Wait - Search element:
+        WebElement inputEmail = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(pizzatempoPage.inputEmail));
+            }
+        });
+        inputEmail.sendKeys("Mirt12@yandex.ru");
+        WebElement inputPassword = driver.findElement(By.xpath(pizzatempoPage.inputPassword));
+        inputPassword.sendKeys("");
+        WebElement btnSearch = driver.findElement(By.xpath(pizzatempoPage.btnSearch));
+        btnSearch.click();
+        //toDo
+        driver.quit();
+    }
+
+    @Test
+    public void enterWithValidEmailAndAnyPassword() {
+        //open browser incognito and w/o cash:
+        options.addArguments("--incognito");
+        options.addArguments("--disable-cache");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.pizzatempo.by");
+        //fluent Wait- Set up:
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(30L))
+                .pollingEvery(Duration.ofSeconds(5L))
+                .ignoring(NoSuchElementException.class);
+        //fluent Wait - Search element:
+        WebElement inputEmail = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath(pizzatempoPage.inputEmail));
+            }
+        });
+        inputEmail.sendKeys("Mirt12@yandex.ru");
+        WebElement inputPassword = driver.findElement(By.xpath(pizzatempoPage.inputPassword));
+        inputPassword.sendKeys("ZZZZZZZZ");
+        WebElement btnSearch = driver.findElement(By.xpath(pizzatempoPage.btnSearch));
+        btnSearch.click();
+        //toDo
+        driver.quit();
+    }
 }
