b3646b5ec73d7865a7a5c56a26567bab23ef364b
Mirt12 <Mirt12@yandex.ru>
2023-06-25 16:25:00 +0200

    add test Open Login Form to BolCome

--- src/main/java/by/itacademy/tatjana/balashevich/pages/BolComPage.java
+++ src/main/java/by/itacademy/tatjana/balashevich/pages/BolComPage.java
@@ -19,5 +19,24 @@ public class BolComPage {
         String homePageAcceptAllCookiesBtnLocator ="//button[@id='js-first-screen-accept-all-button']";
         WebElement submitCookiesBtn = driver.findElement(By.xpath(homePageAcceptAllCookiesBtnLocator));
         submitCookiesBtn.click();
+        //to check get response
     }
+    @Test
+    public void openLoginForm(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--disable-cache");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://www.bol.com/nl/nl/");
+        String homePageAcceptAllCookiesBtnLocator ="//button[@id='js-first-screen-accept-all-button']";
+        WebElement submitCookiesBtn = driver.findElement(By.xpath(homePageAcceptAllCookiesBtnLocator));
+        submitCookiesBtn.click();
+        //open LoginForm:
+        String homePageInloggenLinkLocator = "//header//span[@class='u-show-block@screen-xl-up' and contains(text(),'Inloggen')]";
+        WebElement loginFormLink = driver.findElement(By.xpath(homePageInloggenLinkLocator));
+        loginFormLink.click();
+
+    }
+
 }
